0fc041a4b3e60bdf0161b7e74547029fef5f873c
Sanisinh <esparkbiz.sanisinh@gmail.com>
2023-05-10 19:17:55 +0530

    add useContext hook

--- src/pages/viewData/Table.js
+++ src/pages/viewData/Table.js
@@ -8,7 +8,9 @@ const months = [`Jan ${year}`, `Feb ${year}`, `Mar ${year}`, `Apr ${year}`, `May
 const Table = (props) => {
 
     const records = props.records;
+
     const [data, setData] = useState(records)
+    console.log(data);
     const [sortedField, setSortedField] = useState({});
 
     useEffect(() => {
@@ -135,108 +137,120 @@ const Table = (props) => {
 
     const search = (e) => {
 
-        
+
         if (e.target.value === "") {
-            
+
             setData(records)
         } else {
-           
-            
-            
-            let temp1 = records.filter((i) => i.tdate.toLowerCase().includes(e.target.value.toLowerCase()) ||
-                i.ttype.toLowerCase().includes(e.target.value.toLowerCase()) || i.monthYear.toLowerCase().includes(e.target.value.toLowerCase()) ||
-                i.amount.toLowerCase().includes(e.target.value.toLowerCase()) || i.fromAccount.toLowerCase().includes(e.target.value.toLowerCase()) ||
+
+
+
+            let temp1 = records.filter((i) =>
+                i.tdate.toLowerCase().includes(e.target.value.toLowerCase()) || i.ttype.toLowerCase().includes(e.target.value.toLowerCase())
+                || i.monthYear.toLowerCase().includes(e.target.value.toLowerCase()) || i.amount.toString().toLowerCase().includes(e.target.value.toString().toLowerCase())
+                || i.fromAccount.toLowerCase().includes(e.target.value.toLowerCase()) ||
                 i.toAccount.toLowerCase().includes(e.target.value.toLowerCase()) || i.remarks.toLowerCase().includes(e.target.value.toLowerCase())
-                )
-                setData(temp1)
+
+            )
+            setData(temp1)
         }
     }
 
+    function drop(arr, id) {
+        // Making a copy with the Array from() method
+        const arrCopy = Array.from(arr);
+
+        const objWithIdIndex = arrCopy.findIndex((obj) => obj.id === id);
+        arrCopy.splice(objWithIdIndex, 1);
+        setData(arrCopy)
+        return arrCopy;
+    }
 
     return <>
         <div className="search">
             <input type="text" placeholder="Search.." name="search" onInput={search} />
         </div>
         {
-            records1.length > 0?<>
-            <table>
-            <thead>
-                <tr>
-                    <th onClick={() => sort('tdate')}>Transection-date</th>
-                    <th onClick={() => sort('ttype')}>Transection-Type</th>
-                    <th onClick={() => sort('monthYear')}>Month-Year</th>
-                    <th onClick={() => sort('amount')}>Amount</th>
-                    <th onClick={() => sort('fromAccount')}>From-Account</th>
-                    <th onClick={() => sort('toAccount')}>To-Account</th>
-                    <th onClick={() => sort('remarks')}>Remarks</th>
-                    <th>Receipt</th>
-                    <th colSpan={2}>Action</th>
-                </tr>
-            </thead>
-            {
-                records1.map((data, index) =>
-                    <tbody key={index}>
-                        <tr >
-                            <td>{data.tdate}</td>
-                            <td>{data.ttype}</td>
-                            <td>{data.monthYear}</td>
-                            <td>{Intl.NumberFormat("en-US", {
-                                style: "currency",
-                                currency: "INR",
-                            }).format(data.amount)}</td>
-                            <td>{data.fromAccount}</td>
-                            <td>{data.toAccount}</td>
-                            <td>{data.remarks}</td>
-                            <td><img src={data.receipt} width={50} height={50} alt="" /></td>
-                            <td><Link to={`/transection/${data.id}`}>Edit</Link></td>
-                            <td><Link to={`/view-data/${data.id}`}>View</Link></td>
+            records1.length > 0 ? <>
+                <table>
+                    <thead>
+                        <tr>
+                            <th onClick={() => sort('tdate')}>Transection-date</th>
+                            <th onClick={() => sort('ttype')}>Transection-Type</th>
+                            <th onClick={() => sort('monthYear')}>Month-Year</th>
+                            <th onClick={() => sort('amount')}>Amount</th>
+                            <th onClick={() => sort('fromAccount')}>From-Account</th>
+                            <th onClick={() => sort('toAccount')}>To-Account</th>
+                            <th onClick={() => sort('remarks')}>Remarks</th>
+                            <th>Receipt</th>
+                            <th colSpan={3}>Action</th>
                         </tr>
-                    </tbody>
-                )
-            }
-        </table>
-        <nav>
+                    </thead>
+                    {
+                        records1.map((data, index) =>
+                            <tbody key={index}>
+                                <tr >
+                                    <td>{data.tdate}</td>
+                                    <td>{data.ttype}</td>
+                                    <td>{data.monthYear}</td>
+                                    <td>{Intl.NumberFormat("en-US", {
+                                        style: "currency",
+                                        currency: "INR",
+                                    }).format(data.amount)}</td>
+                                    <td>{data.fromAccount}</td>
+                                    <td>{data.toAccount}</td>
+                                    <td>{data.remarks}</td>
+                                    <td><img src={data.receipt} width={50} height={50} alt="" /></td>
+                                    <td><Link to={`/transection/${data.id}`}>Edit</Link></td>
+                                    <td><Link to={`/view-data/${data.id}`}>View</Link></td>
+                                    <td><i onClick={() => drop(data, data.id)} className="fa fa-trash-o" style={{ fontSize: 25 }}></i></td>
+                                </tr>
+                            </tbody>
+                        )
+                    }
+                </table>
+                <nav>
 
-            <ul className="pagination">
-                <li className="">
-                    <span
-                        className={`page-link ${currentPage === 1 ? "disable" : ""}`}
-                        onClick={prePage}
-                        style={{ cursor: "pointer" }}
-                    >
-                        Prev
-                    </span>
-                </li>
-                {numbers.map((n, index) => (
-                    <li
-                        className={`page-item ${currentPage === n ? "active" : ""}`}
-                        key={index}
-                    >
-                        <span
-                            className="page-link"
-                            onClick={() => changeCurrentPage(n)}
-                            style={{ cursor: "pointer" }}
-                        >
-                            {n}
-                        </span>
-                    </li>
-                ))}
-                <li className="page-item">
-                    <span
-                        className={`page-link ${currentPage === totalPages ? "disable" : ""}`}
-                        onClick={NextPage}
-                        style={{ cursor: "pointer" }}
-                    >
-                        Next
-                    </span>
-                </li>
-            </ul>
-        </nav>
-            
-            </>:<><h1>No data Found</h1></>
+                    <ul className="pagination">
+                        <li className="">
+                            <span
+                                className={`page-link ${currentPage === 1 ? "disable" : ""}`}
+                                onClick={prePage}
+                                style={{ cursor: "pointer" }}
+                            >
+                                Prev
+                            </span>
+                        </li>
+                        {numbers.map((n, index) => (
+                            <li
+                                className={`page-item ${currentPage === n ? "active" : ""}`}
+                                key={index}
+                            >
+                                <span
+                                    className="page-link"
+                                    onClick={() => changeCurrentPage(n)}
+                                    style={{ cursor: "pointer" }}
+                                >
+                                    {n}
+                                </span>
+                            </li>
+                        ))}
+                        <li className="page-item">
+                            <span
+                                className={`page-link ${currentPage === totalPages ? "disable" : ""}`}
+                                onClick={NextPage}
+                                style={{ cursor: "pointer" }}
+                            >
+                                Next
+                            </span>
+                        </li>
+                    </ul>
+                </nav>
+
+            </> : <><h1>No data Found</h1></>
         }
-       
-        
+
+
 
     </>
 }
